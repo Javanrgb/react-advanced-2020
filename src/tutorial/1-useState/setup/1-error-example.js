b88ae8b553f42d10e272874408841ad78d017024
javan <ochiengjavan133@gmail.com>
2023-02-25 15:34:36 +0300

    last commit

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+
+
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title= 'Random Tile';
+
+  const handleClick = () =>{
+    console.log(title);
+  }
+  return <>
+  <h3>{title}</h3>
+  <button typpe="button" className="btn" onClick={handleClick} >Change Title</button>
+  </>
 };
 
 export default ErrorExample;
