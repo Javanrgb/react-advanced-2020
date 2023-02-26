7516f5e8756cce5eaeea42d2360de217ea97264e
javan <ochiengjavan133@gmail.com>
2023-02-26 15:57:06 +0300

    todays commit

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,16 +6,19 @@ const UseStateObject = () => {
   age:30,
   message:'Hello guys'
 })
+const[name,setName] = useState('Peter')
+const[age,setAge] = useState(30)
+const[message,setMessage] = useState('Hello')
 const myHandler=()=>{
-setPerson({...person, message:'The new message'})
-
+//setPerson({...person, message:'The new message'})
+setMessage('Hello and welcome')
 }
 
   return(
     <>
-    <h2>{person.name}</h2>
-    <h3>{person.age}</h3>
-    <h3>{person.message}</h3>
+    <h2>{name}</h2>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
     <button name='btn' onClick={myHandler}>Change Message</button>
     </>
   )
